f99fff64f48626954c4e2f7af137de17e9be072b
Ashwani Rathee <ab669522@gmail.com>
2021-06-16 09:19:13 +0530

    Tests and .gitignore update

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 using HTTP, JSON
 
 global CREDENTIALS = ENV["CREDENTIALS"]
+init("test/fir-jl-457eb-firebase-adminsdk-40928-0efd6a89e7.json")
 # headers = Dict(
 #     "Authorization" =>
 #         "Bearer ya29.c.Kp8BAwh19dsnHN7yPkhlb3toQleIQuAQ94YlV-aXs6ijDEvEKtvKmoNiXw9Fg7u-EjpHjusuu_k9nIaW7tFohhW97fPTx7G0KowT-R0nwI52XdvHrG-TrRAZOjC38eiu_eaPgXTh2wxHUDH-sVPpvA5Yzv8DgCoCRTUboxa3ajDVhRiyvM7uOiFcR27k6eoMrOmk_EBTabpODRLhtIeKG10O",
