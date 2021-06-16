55847e7bc7810e851328ad24c737decc8abce3e7
Ashwani Rathee <ab669522@gmail.com>
2021-06-16 08:39:38 +0530

    Test workflow fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Firebase
 using Test
 using HTTP, JSON
 
-init("./fir-jl-457eb-firebase-adminsdk-40928-0efd6a89e7.json");
+CREDENTIALS = ENV["CREDENTIALS"]
 # headers = Dict(
 #     "Authorization" =>
 #         "Bearer ya29.c.Kp8BAwh19dsnHN7yPkhlb3toQleIQuAQ94YlV-aXs6ijDEvEKtvKmoNiXw9Fg7u-EjpHjusuu_k9nIaW7tFohhW97fPTx7G0KowT-R0nwI52XdvHrG-TrRAZOjC38eiu_eaPgXTh2wxHUDH-sVPpvA5Yzv8DgCoCRTUboxa3ajDVhRiyvM7uOiFcR27k6eoMrOmk_EBTabpODRLhtIeKG10O",
