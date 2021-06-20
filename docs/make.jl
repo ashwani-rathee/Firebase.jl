8d2ae33cd47367ebd598ebf5b0773be1bdbb530b
Ashwani Rathee <ab669522@gmail.com>
2021-06-20 12:52:48 +0530

    Hoping documentation works

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,27 @@
-push!(LOAD_PATH,"../src/")
-using Documenter
-# Inside make.jl
 
-makedocs(sitename="Documentation",
-        pages=[
-            "Home" => "index.md"],
-            format = Documenter.HTML(
-                prettyurls = Base.get(ENV, "CI", nothing) == "true"
-            ))
-deploydocs(repo = "github.com/ashwani-rathee/Firebase.jl", push_preview    = true)
+push!(LOAD_PATH,"../src/")
+using Firebase
+using Documenter
+
+DocMeta.setdocmeta!(Firebase, :DocTestSetup, :(using Firebase); recursive=true)
+
+makedocs(;
+    modules=[Firebase],
+    authors="Panquesito7",
+    repo="github.com/ashwani-rathee/Firebase.jl/blob/{commit}{path}#{line}",
+    sitename="Firebase.jl",
+    format=Documenter.HTML(;
+        prettyurls=Base.get(ENV, "CI", "false") == "true",
+        canonical="https://ashwani-rathee.github.io/Firebase.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/ashwani-rathee/Firebase.jl",
+    devbranch="main",
+    push_preview = true
+) 
