ec820b56e314e705613cc0c1c4639b233a759f90
Ashwani Rathee <ab669522@gmail.com>
2021-06-21 22:45:15 +0530

    GET/POST

--- src/cloudstorage.jl
+++ src/cloudstorage.jl
@@ -1,22 +1,49 @@
+using GoogleCloud
 STORE_URL = nothing
-function cloudstore_init()
+
+function cloudstore_init(url = "https://firebasestorage.googleapis.com/v0/b/fir-jl-457eb.appspot.com/o")
     global STORE_URL = url
     println("Cloud Storage set:", STORE_URL)
 end
 
 """
+cloudstore_sendfile(file2upload="test/onedance.mp3")
 
+Upload file to cloud Storage
+
+# Example
+
+```julia
+cloudstore_init("[STORE_URL]")
+cloudstore_sendfile("[FILE_LOCATION", "[CONTENT_TYPE", "[STORAGE_LOCATION]")
+
+#=
+Dict{String, Any} with 15 entries:
+  "md5Hash"            => "OSGtKjo4CNeMz49k+71MaA=="
+  "name"               => "audio/data.mp3"
+  "downloadTokens"     => "4f9be41c-bd5c-412a-a40d-1a5898812c52"
+  "metageneration"     => "1"
+  "bucket"             => "fir-jl-457eb.appspot.com"
+  "timeCreated"        => "2021-06-21T16:59:25.222Z"
+  "contentType"        => "audio/mp3"
+  "updated"            => "2021-06-21T16:59:25.222Z"
+  "etag"               => "CJyF98OZqfECEAE="
+  "size"               => "3556629"
+  "generation"         => "1624294765216412"
+  "storageClass"       => "STANDARD"
+  "crc32c"             => "Qu2WQw=="
+  "contentDisposition" => "inline; filename*=utf-8''data.mp3"
+  "contentEncoding"    => "identity"
+=#
+```
 
 """
-function cloudstore_sendfile(file2upload="test/onedance.mp3")
+function cloudstore_sendfile(file2upload="test/onedance.mp3", content_type="audio/mp3", storage_location ="audio%2Fdata.mp3")
     file_binary = open(file2upload, "r")
-    s = read(file_binary, String)      
-    header = Dict("Content-Type" => "audio/mp3", auth_header())
+    s = read(file_binary, String)
+    header = Dict("Content-Type" => "$content_type", auth_header())
     project_id = projectid()
-    final_url = "https://firebasestorage.googleapis.com/v0/b/fir-jl-457eb.appspot.com/o/audio%2Fdata.mp3"
-    final_url = "https://firebasestorage.googleapis.com/v0/b/fir-jl-457eb.appspot.com/o/data.mp3"
-    # body = JSON.json(s)
-    # println("Body:", body)
+    final_url = "$(STORE_URL)/$storage_location"
     body = s
     println("FINAL_URL:", final_url)
     http_response = HTTP.post(
@@ -29,9 +56,12 @@ function cloudstore_sendfile(file2upload="test/onedance.mp3")
 
 end
 
+"""
+cloudstorage_get()
 
+"""
 function cloudstorage_get()
-    # res = HTTP.get("https://firebasestorage.googleapis.com/v0/b/fir-jl-457eb.appspot.com/o/242160__xserra__cello-phrase.wav?alt=media&token=878c7715-61cf-46b5-a9c0-a790300d8c43")
+    res = HTTP.get("https://firebasestorage.googleapis.com/v0/b/fir-jl-457eb.appspot.com/o/242160__xserra__cello-phrase.wav?alt=media&token=878c7715-61cf-46b5-a9c0-a790300d8c43")
 
     # julia> out =open("data.mp3","w")
     # IOStream(<file data.mp3>)
